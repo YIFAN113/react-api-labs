8ae18930164b762c9a83cdfaae46495be89ca47f
Yifan Gu <20095253@mail.wit.ie>
2023-11-25 00:42:41 +0800

    Mongo lab Exercises done

--- tasky-api/api/users/userModel.js
+++ tasky-api/api/users/userModel.js
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
-
+const passwordRegex =/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
 const UserSchema = new Schema({
   username: { type: String, unique: true, required: true},
-  password: {type: String, required: true }
+  password: {type: String, 
+             required: true,
+             match:[passwordRegex] }
 });
 
 export default mongoose.model('User', UserSchema);
